f7ad3b8a271c464e7a9de79bd3b4f2933ed18acc
CarpeDMT <jrojas989@gmail.com>
2020-02-26 00:39:18 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,21 @@ var APP = {
 		var loader = new THREE.ObjectLoader();
 		var camera, scene, renderer;
 
+		var listener = new THREE.AudioListener();
+		camera.add( listener );
+
+		// create a global audio source
+		var sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+		var audioLoader = new THREE.AudioLoader();
+		audioLoader.load( 'sounds/ambient.ogg', function( buffer ) {
+		sound.setBuffer( buffer );
+		sound.setLoop( true );
+		sound.setVolume( 0.5 );
+		sound.play();
+});
+		
 		var events = {};
 
 		var dom = document.createElement( 'div' );
